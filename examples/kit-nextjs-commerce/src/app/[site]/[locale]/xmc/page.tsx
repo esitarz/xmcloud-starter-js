af2f00e90a0d174978a70901b7be826f149b3fa0
Crhistian Ramirez <crhistian.ramirez@sitecore.com>
2026-09-09 13:29:09 -0500

    OCP-1251 Remove legacy commerce dispatcher

--- examples/kit-nextjs-commerce/src/app/[site]/[locale]/xmc/page.tsx
+++
@@ -1,9 +0,0 @@
-import { XmcTokenLab } from '@/features/xmc/token-lab';
-
-export default function XmcPage() {
-  return (
-    <main className="px-4 py-10 sm:px-8 lg:px-10">
-      <XmcTokenLab />
-    </main>
-  );
-}
